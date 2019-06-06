80d1d43a1665dd099ab10d25d94f516a4ceacfa6
Jamie Vullo <jvullo@protonmail.com>
2019-06-06 14:52:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,28 @@
-class Author 
-  attr_accessor :name
-  @@post_count = 0
+class Author
 
-   def initialize(name)
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+  
+  def initialize(name)
     @name = name
     @posts = []
   end
 
-   def posts
-    @posts
-  end
-
-   def add_post(post)
+  def add_post(post)
     @posts << post
     post.author = self
     @@post_count += 1
   end
 
-   def add_post_by_title(post_title)
-    post = Post.new(post_title)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.author = self
     @@post_count += 1
   end
 
-   def self.post_count
+  def self.post_count
     @@post_count
   end
-
- end 
+end
