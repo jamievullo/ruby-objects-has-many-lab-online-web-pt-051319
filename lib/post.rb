29f5885a6a482c70de6d5407a964eefd1f67e431
Jamie Vullo <jvullo@protonmail.com>
2019-06-06 14:53:52 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,11 @@ class Post
 
   attr_accessor :title, :author
   
+  @@all = []
+  
   def initialize(title)
     @title = title
+    @@all << self 
   end
 
   def author_name
@@ -13,4 +16,9 @@ class Post
       @author.name
     end
   end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
